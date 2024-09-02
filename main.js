e805ca8b9c0e58212738bf07b2f3c022a4e821f2
Sup-reme1 <abdulbasitolalekan20@gmail.com>
2024-09-02 08:49:51 +0100

    Cleaned main.js with functions and comment

--- main.js
+++ main.js
@@ -28,31 +28,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     const productsTag = products.map(product => product.tag);
-    // console.log(productsTag);
 
     productsTag.forEach(tag => {
-        // var cart = document.getElementById(tag);
-        // var productDetails = document.querySelector(`#${tag} img`)
         var cart = document.querySelector(`.${tag} .add-to-cart`);
-        // console.log(cart);
         
         // Check for the clicked cart and display count
         cart.addEventListener('click', ()=>{
             console.log(`${tag} was clicked`);
 
             products.forEach(product => {
-                // console.log(tag);
                 if (product.tag === tag) {
                   if (product.iscart === false) {
                     // Adjust the product properties when clicked and update object properties
                     product.iscart = true;                    
                     addCart(product, cart);
+                  
+                    updateCountContainer('order-cart')    // Update Aside container            
 
-                    // Update Aside container
-                    updateCountContainer('order-cart')                
-
-                  } 
-                //   else product.iscart = false;
+                  }
                 }
             });
 
@@ -60,15 +53,12 @@ document.addEventListener('DOMContentLoaded', () => {
         })
 
         var incrementIcon = document.querySelector(`.${tag} .increment`);
-        // console.log(incrementIcon);
         incrementIcon.addEventListener('click', e => {
             products.forEach(product => {
-                // console.log(product.tag);
                 if(product.tag === tag){
                     // Increment Cart
-                    addCart(product, cart);
-                    // Update Aside container
-                    updateCountContainer('order-cart');
+                    addCart(product, cart);                   
+                    updateCountContainer('order-cart');      // Update Aside container
                 }
             })
         })
@@ -76,30 +66,36 @@ document.addEventListener('DOMContentLoaded', () => {
         var decrementIcon = document.querySelector(`.${tag} .decrement`);
         decrementIcon.addEventListener('click', e => {
             products.forEach(product => {
-                // console.log(product.tag);
                 if(product.tag === tag){
                     // Decrement Cart
-                    removeCart(product, cart);
-                    // Update Aside container
-                    updateCountContainer('order-cart');
+                    removeCart(product, cart);                    
+                    updateCountContainer('order-cart');     // Update Aside container
 
                 };                    
             })
         })
 
-        confirmOrder = document.querySelector('#btn');
-        confirmOrder.addEventListener('click', ()=> {
-          confirmOrderSection = document.querySelector('.order-confirm-section');
-          fillConfirmOrder();
-          updateCountContainer('order-confirm-section');
-          confirmOrderSection.style.display = 'block';
-        })
+        // confirmOrder = document.querySelector('#btn');
+        // confirmOrder.addEventListener('click', ()=> {
+        //   confirmOrderSection = document.querySelector('.order-confirm-section');
+        //   fillConfirmOrder();
+        //   updateCountContainer('order-confirm-section');
+        //   confirmOrderSection.style.display = 'block';
+        // })
 
     })
 
 
 
     function fillConfirmOrder(){
+        // li.setAttribute("class", "items message-items");
+        // div.setAttribute('class', 'tumbnails-flex');
+        // tumbnailImg = document.createElement('img');
+        // tumbnailImg.setAttribute('src', 'assets/images/image-tiramisu-thumbnail.jpg')
+        // tumbnailImg.setAttribute('class', 'tumbnails');
+        // div.append(tumbnailImg);
+        
+
         const total = document.querySelectorAll('.order-amount')[1];
         console.log(total);
         total.textContent = `$${sumTotal()}`;
@@ -115,8 +111,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
         orderContainer.forEach(product => {
             const asideEmptyContainer = document.querySelector('.empty-cart');
-            // const nextSibling = asideEmptyContainer.nextElementSibling;
-            // const asideOrderContainer = document.querySelector('.order-cart');
             const asideOrderContainer = document.querySelector(`.${containerId}`);
             const orderSpan = asideOrderContainer.querySelector('span');
 
@@ -126,7 +120,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Display the total count of items
             count += product.count;
-            // console.log(nextSibling);
             if(containerId === 'order-cart') orderSpan.textContent = count;
 
             // Selecting List container
@@ -135,131 +128,108 @@ document.addEventListener('DOMContentLoaded', () => {
             // Check for Existing list
             var existingLi = ulList.querySelector(`li[data-id = '${product.tag}']`);
             if (existingLi) {
-              // Select the Elements in existingLi
-              const pElement = existingLi.querySelector("p");
-              const h2Element = existingLi.querySelector("h2");
-              const bElement = existingLi.querySelector("b");
-
-              // If all the product is removed, Exit the container
-              if (product.count === 0) {
-                existingLi.remove();
-                product.iscart = false;
-                checkContainer(asideEmptyContainer, asideOrderContainer);
-              }
-
-              // Write inside the Elements in existingLi
-              pElement.textContent = `${product.count}x`;
-              h2Element.textContent = `@$${product.price}`;
-              bElement.textContent = `$${total(product)}`;
+                checkExistingList(existingLi, product); // Function that update existingLi element
             } else {
-
-              // Creating a li element for the selected product
-              var li = document.createElement("li");
-              var div = document.createElement("div");
-            //   Conditional statement to check which container to populate
-              if (containerId === 'order-cart'){
-                  li.setAttribute("class", "items");
-            } else{
-                li.setAttribute("class", "items message-items");
-                // div.setAttribute('class', 'tumbnails-flex');
-                // tumbnailImg = document.createElement('img');
-                // tumbnailImg.setAttribute('src', 'assets/images/image-tiramisu-thumbnail.jpg')
-                // tumbnailImg.setAttribute('class', 'tumbnails');
-                // div.append(tumbnailImg);
-            }
-
-              li.setAttribute("data-id", product.tag);
-              
-              var h1Element = document.createElement("h1");
-
-              var divQty = document.createElement("div");
-              divQty.setAttribute("class", "items-qty-price");
-              var pElement = document.createElement("p");
-              var h2Element = document.createElement("h2");
-              h2Element.setAttribute("class", "item-price");
-              var bElement = document.createElement("b");
-              bElement.setAttribute("class", "item-total");
-
-              var imgElement = document.createElement("img");
-              imgElement.setAttribute(
-                "src", "assets/images/icon-remove-item.svg"
-              );
-
-              // Creating Data element for the list
-              var h1Data = document.createTextNode(product.name);
-              var pData = document.createTextNode(`${product.count}x`);
-              var h2Data = document.createTextNode(`@$${product.price}`);
-              var bData = document.createTextNode(`$${total(product)}`);
-
-              // Appending Textnode to elements
-              h1Element.append(h1Data);
-              pElement.append(pData);
-              h2Element.append(h2Data);
-              bElement.append(bData);
-
-              // Appending element to DIVQTY
-              divQty.append(pElement);
-              divQty.append(h2Element);
-              divQty.append(bElement);
-
-              // Appending product data to DIV container
-              div.append(h1Element);
-              div.append(divQty);
-
-              // Appending Elements to Li
-              li.append(div);
-              li.append(imgElement);
-
               // Appending the Li to ul list
+              li = createProductList(product);
               ulList.append(li);
-
-            //   Event listener for he cancle img created in the list
-              var cart = document.querySelector(`.${product.tag} .add-to-cart`);
-              li.querySelector('img').addEventListener('click', ()=>{
-                product.iscart = false;
-                product.count = 0;
-                li.remove();
-                updateCart(product, cart);
-                checkContainer(asideEmptyContainer, asideOrderContainer);
-              })
-
-
-              
-              
+              sumTotal();
             }
 
-            // Total section
-            // get the class id of total section and write inside the total element
-            const orderTotal = document.querySelector('.order-total');
-            var h1Total = orderTotal.querySelector('h1');
-            h1Total.textContent = `$${sumTotal()}`;
-            orderTotal.append(h1Total);
-
+            // Event listener for he cancle img created in the list
+            var cart = document.querySelector(`.${product.tag} .add-to-cart`);
+            var list = ulList.querySelector(`li[data-id = '${product.tag}']`);
+            list.querySelector('img').addEventListener('click', ()=>{
+                deleteList(product, list, cart);    // Function that delete existingLi element
+                checkContainer(asideEmptyContainer, asideOrderContainer);
+            })
+            
         })
     }
 
-    function checkCancleBtn(tag){
-        // // const orderContainer = document.querySelector('.order-cart');
-        // var orderList = document.querySelector(`li[data-id = '${tag}']`);
-        // var cancleBtn = orderList.querySelector(img);
-        // console.log(cancleBtn);
-        // cancleBtn.addEventListener('click', ()=>{
-        //     orderList.remove();
-        // })
-        // console.log(orderList);
-        // const cancleBtn = orderList.querySelector('img');
-        // cancleBtn.addEventListener('click', ()=>{
-        //   console.log('here');
-        //   console.log(product.name);
-        //   existingLi.remove();
-        // })
+    // Function that delete existingLi element
+    function deleteList(product, list, cart){
+        product.iscart = false;
+        product.count = 0;
+        list.remove();
+        updateCart(product, cart);
+        sumTotal(); // Function to sum overall total for the Aside section
+    }
+
+    // Function that update existingLi element
+    function checkExistingList(existingLi, product){
+        // Select the Elements in existingLi
+        const pElement = existingLi.querySelector("p");
+        const h2Element = existingLi.querySelector("h2");
+        const bElement = existingLi.querySelector("b");
+
+        // If all the product is removed, Exit the container
+        if (product.count === 0) {
+        existingLi.remove();
+        product.iscart = false;
+        checkContainer(asideEmptyContainer, asideOrderContainer);
+        }
+
+        // Write inside the Elements in existingLi
+        pElement.textContent = `${product.count}x`;
+        h2Element.textContent = `@$${product.price}`;
+        bElement.textContent = `$${total(product)}`;
+        sumTotal();
+    }
+    // function that create li element for the selected product and return the li element
+    function createProductList(product){
+        var li = document.createElement("li");
+        var div = document.createElement("div");            
+        li.setAttribute("class", "items");
+        li.setAttribute("data-id", product.tag);
+        
+        var h1Element = document.createElement("h1");
+
+        var divQty = document.createElement("div");
+        divQty.setAttribute("class", "items-qty-price");
+        var pElement = document.createElement("p");
+        var h2Element = document.createElement("h2");
+        h2Element.setAttribute("class", "item-price");
+        var bElement = document.createElement("b");
+        bElement.setAttribute("class", "item-total");
+
+        var imgElement = document.createElement("img");
+        imgElement.setAttribute(
+          "src", "assets/images/icon-remove-item.svg"
+        );
+
+        // Creating Data element for the list
+        var h1Data = document.createTextNode(product.name);
+        var pData = document.createTextNode(`${product.count}x`);
+        var h2Data = document.createTextNode(`@$${product.price}`);
+        var bData = document.createTextNode(`$${total(product)}`);
+
+        // Appending Textnode to elements
+        h1Element.append(h1Data);
+        pElement.append(pData);
+        h2Element.append(h2Data);
+        bElement.append(bData);
+
+        // Appending element to DIVQTY
+        divQty.append(pElement);
+        divQty.append(h2Element);
+        divQty.append(bElement);
+
+        // Appending product data to DIV container
+        div.append(h1Element);
+        div.append(divQty);
+
+        // Appending Elements to Li
+        li.append(div);
+        li.append(imgElement);
+
+        return li;
     }
 
     // function to check if the aside section is removed entirely 
     function checkContainer(asideEmptyContainer, asideOrderContainer){
         const checkIstrue = products.filter(product => product.iscart === true); 
-        // Check if OrderContainer array become empty
-        console.log(checkIstrue);
+        // Check if OrderContainer array become empty        
         if(checkIstrue.length === 0){
             // Display and hide the aside container
             asideEmptyContainer.style.display = 'grid';
@@ -275,7 +245,14 @@ document.addEventListener('DOMContentLoaded', () => {
         totalSum.forEach(product =>{
             sum += total(product);
         });
-        return sum;
+
+        // Total section
+        // get the class id of total section and write inside the total element
+        const orderTotal = document.querySelector('.order-total');
+        var h1Total = orderTotal.querySelector('h1');
+        h1Total.textContent = `$${sum}`;
+        orderTotal.append(h1Total);
+        // return sum;
     };
 
     // Function to sum each total of products in the Aside section
